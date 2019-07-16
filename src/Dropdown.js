48414856ca4de92a9f44dc7075d3eb8985ac036f
dandelionadia <nadia.ridko@gmail.com>
2019-07-16 22:27:28 +0200

    adds dropdown

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -7,19 +7,39 @@ class Dropdown extends React.Component {
         this.state = {
             isOpen: false
         }
+        this.handleTriggerClick = this.handleTriggerClick.bind(this);
+
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
     }
 
     handleTriggerClick = () => {
-        const isOpen = this.state.isOpen
-        this.setState({
-            isOpen: !isOpen
-        })
+        // const isOpen = this.state.isOpen
+        // this.setState({
+        //     isOpen: !isOpen
+        // })
+        this.setState(prevState => ({
+            isOpen: !prevState.isOpen
+        }))
+
+        if (!this.state.isOpen) {
+            document.addEventListener('click', this.handleOutsideClick, false);
+        } else {
+            document.removeEventListener('click', this.handleOutsideClick, false);
+        }
+    }
+
+    handleOutsideClick = (e) => {
+        // ignore clicks on the component itself
+        if (this.node.contains(e.target)) {
+            return;
+        }
+        this.handleTriggerClick();
     }
 
     render() {
         const { isOpen } = this.state
         return (
-            <div className="dropdown__wrapper" >
+            <div className="dropdown__wrapper" ref={node => { this.node = node; }} >
                 {this.props.trigger({ onClick: this.handleTriggerClick })}
                 {isOpen &&
                     < div className="dropdown" >
